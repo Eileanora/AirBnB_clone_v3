824926861a7897aa336f00dd50560ac18ad4975e
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-10-29 19:42:44 +0200

    pull request

---
+++ api/v1/views/index.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Flask app """
+
+from flask import Flask, jsonify
+from api.v1.views import app_views
+from models import storage
+
+
+@app_views.route('/status', strict_slashes=False)
+def status():
+    """ Returns a JSON """
+    return jsonify({"status": "OK"})
